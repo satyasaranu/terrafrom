87f12548dbb9cd5ed637d0370cb77a2228611425
satyasaranu <saranusys@gmail.com>
2020-08-04 20:38:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
   ami           = "ami-0c5bf07e510b75b11"
   instance_type = "t2.micro"
 }
